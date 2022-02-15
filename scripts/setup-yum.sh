455c99d39905e51191b20f30236ec7837f3e41ae
LeeHor <lee.horn@softwire.com>
2022-02-15 11:15:21 +0000

    use python3 to install poetry

--- scripts/setup-yum.sh
+++ scripts/setup-yum.sh
@@ -1,4 +1,4 @@
 sudo yum install 'python3 >= 3.7'
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
 source $HOME/.poetry/env
 poetry install
