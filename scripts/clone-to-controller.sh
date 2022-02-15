6b78d96bd85b954b55ecf48ca93bb9af201f0e40
LeeHor <lee.horn@softwire.com>
2022-02-15 12:04:25 +0000

    catch no controller set in script

--- scripts/clone-to-controller.sh
+++ scripts/clone-to-controller.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -e
+if [ -z ${CONTROLLER+x} ]; then
+    echo 'No $CONTROLLER variable set. Please set $CONTROLLER to the form user@host'
+    exit 1
+fi
 CURRENT_DIR=$(pwd)
 TEMP=$(mktemp -d)
 git clone file://$CURRENT_DIR --depth 1 $TEMP
